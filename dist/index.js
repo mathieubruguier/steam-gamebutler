7604cda4464e3b4416ab60ecd3e5b00af728997e
Mathieu Bruguier <physicplay@gmail.com>
2017-12-12 10:33:32 -0800

    Delete index.js

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ascii_1 = require("./ascii");
-var database_1 = require("./database");
-ascii_1.printApplicationTitle();
-console.log();
-database_1.Database.initialize();
-//# sourceMappingURL=index.js.map
